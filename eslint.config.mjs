5d3e6357307b081da7e1205080c9697386cfbbf9
BIG <bigguitarsolo@gmail.com>
2024-11-06 00:03:32 +0700

    chore: Switch to Fastify and update configuration files
    Added a `.prettierrc` file to standardize code formatting. Switched
    `NestFactory` to use `NestFastifyApplication` for improved performance
    in `main.ts`. Updated `eslint.config.mjs` for consistent linting rules
    and single quotes.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,27 +1,32 @@
-import js from "@eslint/js";
-import tseslint from "typescript-eslint";
-import unusedImports from "eslint-plugin-unused-imports";
-import jestPlugin from "eslint-plugin-jest";
-import stylistic from "@stylistic/eslint-plugin";
+import js from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import unusedImports from 'eslint-plugin-unused-imports';
+import jestPlugin from 'eslint-plugin-jest';
+import stylistic from '@stylistic/eslint-plugin';
 
 const eslintRecommended = js.configs.recommended;
 const typescriptRecommended = [
   ...tseslint.configs.recommended,
   ...tseslint.configs.stylistic,
+  stylistic.configs.customize({
+    indent: 2,
+    quotes: 'single',
+    semi: true,
+  }),
 ];
 const plugins = {
-  "@typescript-eslint": tseslint.plugin,
-  "unused-imports": unusedImports,
+  '@typescript-eslint': tseslint.plugin,
+  'unused-imports': unusedImports,
   jest: jestPlugin,
-  "@stylistic": stylistic,
+  '@stylistic': stylistic,
 };
 const typescriptResolver = {
   settings: {
     typescript: {
-      "import/resolver": {
+      'import/resolver': {
         typescript: {
           alwaysTryTypes: true,
-          project: "./tsconfig.json",
+          project: './tsconfig.json',
         },
       },
     },
@@ -31,62 +36,63 @@ const parserOptions = {
   languageOptions: {
     parser: tseslint.parser,
     parserOptions: {
-      project: "./tsconfig.json",
+      project: './tsconfig.json',
     },
   },
 };
 const fileIgnores = {
   ignores: [
-    "**/build/**",
-    "**/dist/**",
-    "**/node_modules/**",
-    "**/*.config.mjs",
-    "**/*.config.js",
-    "package.json",
-    "package-lock.json",
+    '**/build/**',
+    '**/dist/**',
+    '**/node_modules/**',
+    '**/*.config.mjs',
+    '**/*.config.js',
+    'package.json',
+    'package-lock.json',
   ],
 };
 const rules = {
-  "no-unused-vars": "error",
-  "no-undef": "error",
-  "no-return-await": "error",
-  "unused-imports/no-unused-imports": "error",
-  "unused-imports/no-unused-vars": [
-    "warn",
+  'no-unused-vars': 'error',
+  'no-undef': 'off',
+  'no-return-await': 'error',
+  'unused-imports/no-unused-imports': 'error',
+  'unused-imports/no-unused-vars': [
+    'warn',
     {
-      vars: "all",
-      varsIgnorePattern: "^_",
-      args: "after-used",
-      argsIgnorePattern: "^_",
+      vars: 'all',
+      varsIgnorePattern: '^_',
+      args: 'after-used',
+      argsIgnorePattern: '^_',
     },
   ],
-  "@typescript-eslint/no-explicit-any": "error",
-  "@stylistic/indent": ["error", 2],
-  "@stylistic/lines-between-class-members": ["error", "always"],
-  "@stylistic/padding-line-between-statements": [
-    "error",
-    { blankLine: "always", prev: "*", next: "return" },
-    { blankLine: "always", prev: ["const", "let", "var"], next: "*" },
+  '@typescript-eslint/no-explicit-any': 'error',
+  '@stylistic/indent': ['error', 2],
+  '@stylistic/operator-linebreak': 'off',
+  '@stylistic/lines-between-class-members': ['error', 'always'],
+  '@stylistic/padding-line-between-statements': [
+    'error',
+    { blankLine: 'always', prev: '*', next: 'return' },
+    { blankLine: 'always', prev: ['const', 'let', 'var'], next: '*' },
     {
-      blankLine: "any",
-      prev: ["const", "let", "var"],
-      next: ["const", "let", "var"],
+      blankLine: 'any',
+      prev: ['const', 'let', 'var'],
+      next: ['const', 'let', 'var'],
     },
-    { blankLine: "always", prev: "directive", next: "*" },
-    { blankLine: "any", prev: "directive", next: "directive" },
-    { blankLine: "always", prev: ["case", "default", "if"], next: "*" },
-    { blankLine: "always", prev: "*", next: "try" },
-    { blankLine: "always", prev: "try", next: "*" },
-    { blankLine: "always", prev: "*", next: "throw" },
+    { blankLine: 'always', prev: 'directive', next: '*' },
+    { blankLine: 'any', prev: 'directive', next: 'directive' },
+    { blankLine: 'always', prev: ['case', 'default', 'if'], next: '*' },
+    { blankLine: 'always', prev: '*', next: 'try' },
+    { blankLine: 'always', prev: 'try', next: '*' },
+    { blankLine: 'always', prev: '*', next: 'throw' },
   ],
 };
 const jsFiles = {
-  files: ["**/.js"],
+  files: ['**/.js'],
   ...tseslint.configs.disableTypeChecked,
 };
 const testFiles = {
-  files: ["test/**"],
-  rules: { "jest/prefer-expect-assertions": "error" },
+  files: ['test/**'],
+  rules: { 'jest/prefer-expect-assertions': 'error' },
 };
 
 export default [
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+async function bootstrap(): Promise<void> {
+  const app: NestFastifyApplication =
+    await NestFactory.create<NestFastifyApplication>(
+      AppModule,
+      new FastifyAdapter(),
+      {
+        bufferLogs: true,
+      },
+    );
+
   await app.listen(process.env.PORT ?? 3000);
 }
+
+// noinspection JSIgnoredPromiseFromCall
 bootstrap();
