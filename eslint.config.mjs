23001004b84ce8dee569f66b6f022836209e8fb9
BIG <bigguitarsolo@gmail.com>
2024-11-05 23:33:20 +0700

    chore: Add and update ESLint dependencies
    In this commit, several new ESLint-related dependencies have been added,
     while existing ones have been updated. This includes essential plugins
     and configurations to enhance code quality and consistency across the
     project.

---
+++ eslint.config.mjs
@@ -0,0 +1,102 @@
+import js from "@eslint/js";
+import tseslint from "typescript-eslint";
+import unusedImports from "eslint-plugin-unused-imports";
+import jestPlugin from "eslint-plugin-jest";
+import stylistic from "@stylistic/eslint-plugin";
+
+const eslintRecommended = js.configs.recommended;
+const typescriptRecommended = [
+  ...tseslint.configs.recommended,
+  ...tseslint.configs.stylistic,
+];
+const plugins = {
+  "@typescript-eslint": tseslint.plugin,
+  "unused-imports": unusedImports,
+  jest: jestPlugin,
+  "@stylistic": stylistic,
+};
+const typescriptResolver = {
+  settings: {
+    typescript: {
+      "import/resolver": {
+        typescript: {
+          alwaysTryTypes: true,
+          project: "./tsconfig.json",
+        },
+      },
+    },
+  },
+};
+const parserOptions = {
+  languageOptions: {
+    parser: tseslint.parser,
+    parserOptions: {
+      project: "./tsconfig.json",
+    },
+  },
+};
+const fileIgnores = {
+  ignores: [
+    "**/build/**",
+    "**/dist/**",
+    "**/node_modules/**",
+    "**/*.config.mjs",
+    "**/*.config.js",
+    "package.json",
+    "package-lock.json",
+  ],
+};
+const rules = {
+  "no-unused-vars": "error",
+  "no-undef": "error",
+  "no-return-await": "error",
+  "unused-imports/no-unused-imports": "error",
+  "unused-imports/no-unused-vars": [
+    "warn",
+    {
+      vars: "all",
+      varsIgnorePattern: "^_",
+      args: "after-used",
+      argsIgnorePattern: "^_",
+    },
+  ],
+  "@typescript-eslint/no-explicit-any": "error",
+  "@stylistic/indent": ["error", 2],
+  "@stylistic/lines-between-class-members": ["error", "always"],
+  "@stylistic/padding-line-between-statements": [
+    "error",
+    { blankLine: "always", prev: "*", next: "return" },
+    { blankLine: "always", prev: ["const", "let", "var"], next: "*" },
+    {
+      blankLine: "any",
+      prev: ["const", "let", "var"],
+      next: ["const", "let", "var"],
+    },
+    { blankLine: "always", prev: "directive", next: "*" },
+    { blankLine: "any", prev: "directive", next: "directive" },
+    { blankLine: "always", prev: ["case", "default", "if"], next: "*" },
+    { blankLine: "always", prev: "*", next: "try" },
+    { blankLine: "always", prev: "try", next: "*" },
+    { blankLine: "always", prev: "*", next: "throw" },
+  ],
+};
+const jsFiles = {
+  files: ["**/.js"],
+  ...tseslint.configs.disableTypeChecked,
+};
+const testFiles = {
+  files: ["test/**"],
+  rules: { "jest/prefer-expect-assertions": "error" },
+};
+
+export default [
+  eslintRecommended,
+  ...typescriptRecommended,
+  { plugins },
+  typescriptResolver,
+  parserOptions,
+  fileIgnores,
+  { rules },
+  jsFiles,
+  testFiles,
+];
